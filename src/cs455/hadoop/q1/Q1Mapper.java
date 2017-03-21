99b1a6b982ba6162530997702c857c5b0a3c1b37
David Edwards <david.edwards@colostate.edu>
2017-03-21 17:02:42 -0600

    trying out State enum for custom counter

--- src/cs455/hadoop/q1/Q1Mapper.java
+++ src/cs455/hadoop/q1/Q1Mapper.java
@@ -1,6 +1,7 @@
 package cs455.hadoop.q1;
 
 import cs455.hadoop.util.CensusData;
+import cs455.hadoop.util.State;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -14,8 +15,12 @@ public class Q1Mapper extends Mapper<LongWritable, Text, Text, IntWritable>
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
     {
         CensusData data = new CensusData(value.toString());
-
         if (data.getSummaryLevel() == 100)
-            context.write(new Text(data.getStateAbbreviation()), new IntWritable(data.getFemale()));
+        {
+            context.write(new Text(data.getStateAbbreviation() + " - Rent"), new IntWritable(data.getRenterOccupied()));
+            context.write(new Text(data.getStateAbbreviation() + " - Own"), new IntWritable(data.getOwnerOccupied()));
+            context.write(new Text(data.getStateAbbreviation() + " - Total"), new IntWritable(data.getOwnerOccupied() + data.getRenterOccupied()));
+            context.getCounter(State.valueOfAbbreviation(data.getStateAbbreviation())).increment(data.getOwnerOccupied() + data.getRenterOccupied());
+        }
     }
 }
