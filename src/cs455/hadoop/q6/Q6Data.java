aa41dac61f717c0d66f5fb1247860a778d322d5a
David Edwards <david@honeycrisp.local>
2017-04-09 21:22:16 -0600

    Q6 ready to test

--- src/cs455/hadoop/q6/Q6Data.java
+++ src/cs455/hadoop/q6/Q6Data.java
@@ -1,24 +1,89 @@
 package cs455.hadoop.q6;
 
+import cs455.hadoop.util.CensusData;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Writable;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 
 public class Q6Data implements Writable
 {
-    @Override
-    public String toString()
+    private HashMap<String, IntWritable> values;
+
+    public Q6Data()
     {
-        return super.toString();
+        values = new LinkedHashMap<>();
+        values.put("rentUnder100", new IntWritable());
+        values.put("rent100To149", new IntWritable());
+        values.put("rent150To199", new IntWritable());
+        values.put("rent200To249", new IntWritable());
+        values.put("rent250To299", new IntWritable());
+        values.put("rent300To349", new IntWritable());
+        values.put("rent350To399", new IntWritable());
+        values.put("rent400To449", new IntWritable());
+        values.put("rent450To499", new IntWritable());
+        values.put("rent500To549", new IntWritable());
+        values.put("rent550To599", new IntWritable());
+        values.put("rent600To649", new IntWritable());
+        values.put("rent650To699", new IntWritable());
+        values.put("rent700To749", new IntWritable());
+        values.put("rent750To999", new IntWritable());
+        values.put("rentOver1000", new IntWritable());
+        values.put("rentNoCashRent", new IntWritable());
+    }
+
+    public HashMap<String, IntWritable> getValues()
+    {
+        return values;
+    }
+
+    public void set(CensusData censusData)
+    {
+        values.put("rentUnder100", new IntWritable(censusData.getRentUnder100()));
+        values.put("rent100To149", new IntWritable(censusData.getRent100To149()));
+        values.put("rent150To199", new IntWritable(censusData.getRent150To199()));
+        values.put("rent200To249", new IntWritable(censusData.getRent200To249()));
+        values.put("rent250To299", new IntWritable(censusData.getRent250To299()));
+        values.put("rent300To349", new IntWritable(censusData.getRent300To349()));
+        values.put("rent350To399", new IntWritable(censusData.getRent350To399()));
+        values.put("rent400To449", new IntWritable(censusData.getRent400To449()));
+        values.put("rent450To499", new IntWritable(censusData.getRent450To499()));
+        values.put("rent500To549", new IntWritable(censusData.getRent500To549()));
+        values.put("rent550To599", new IntWritable(censusData.getRent550To599()));
+        values.put("rent600To649", new IntWritable(censusData.getRent600To649()));
+        values.put("rent650To699", new IntWritable(censusData.getRent650To699()));
+        values.put("rent700To749", new IntWritable(censusData.getRent700To749()));
+        values.put("rent750To999", new IntWritable(censusData.getRent750To999()));
+        values.put("rentOver1000", new IntWritable(censusData.getRentOver1000()));
+        values.put("rentNoCashRent", new IntWritable(censusData.getRentNoCashRent()));
     }
 
     @Override
-    public void write(DataOutput dataOutput) throws IOException
+    public String toString()
     {
+        String outString = "";
 
+        for (HashMap.Entry<String, IntWritable> entry : values.entrySet())
+        {
+            outString += String.format("|%-25s: %d", entry.getKey(), entry.getValue().get());
+        }
+        outString += String.format("|%-25s: %s", "Median", this.median());
+        return outString;
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException
+    {
+        for (HashMap.Entry<String, IntWritable> entry : values.entrySet())
+        {
+            entry.getValue().write((out));
+        }
     }
 
     @Override
@@ -27,7 +92,50 @@ public class Q6Data implements Writable
 
     }
 
-    public void merge(Q6Data data)
+    public void merge(Q6Data mergeData)
     {
+        for (HashMap.Entry<String, IntWritable> entry : values.entrySet())
+        {
+            int val = entry.getValue().get();
+            val += mergeData.getValues().get(entry.getKey()).get();
+            entry.setValue(new IntWritable(val));
+        }
+
     }
+
+    public String median()
+    {
+        ArrayList<String> allValues = new ArrayList<>();
+        for (HashMap.Entry<String, IntWritable> entry : values.entrySet())
+        {
+            for (int i = 0; i < entry.getValue().get(); i++)
+            {
+                allValues.add(entry.getKey());
+            }
+        }
+
+        int middle = allValues.size() / 2;
+        try
+        {
+
+            if (allValues.size() % 2 == 1)
+            {
+                return allValues.get(middle);
+            } else
+            {
+                if (!allValues.get(middle - 1).equals(allValues.get(middle)))
+                {
+                    return "\nMedian between: " + allValues.get(middle - 1) + " and " + allValues.get(middle);
+                } else
+                {
+                    return allValues.get(middle);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            return "middle: " + e.getMessage();
+        }
+    }
+
 }
--- src/cs455/hadoop/q6/Q6Job.java
+++ src/cs455/hadoop/q6/Q6Job.java
@@ -1,6 +1,70 @@
 package cs455.hadoop.q6;
 
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Q6Job
 {
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            Configuration conf = new Configuration();
+
+            Job job = Job.getInstance(conf, "cdedward Q6 Job");
+            job.setJarByClass(Q6Job.class);
+            job.setMapperClass(Q6Mapper.class);
+            job.setCombinerClass(Q6Reducer.class);
+            job.setReducerClass(Q6Reducer.class);
+
+            job.setMapOutputKeyClass(Text.class);
+            job.setMapOutputValueClass(Q6Data.class);
+
+            job.setOutputKeyClass(Text.class);
+            job.setOutputValueClass(Q6Data.class);
+
+            FileInputFormat.addInputPath(job, new Path(args[0]));
+            FileOutputFormat.setOutputPath(job, new Path(args[1]));
+            job.waitForCompletion(true);
+
+            FileSystem fs = FileSystem.get(conf);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(new Path(args[1] + "/part-r-00000"))));
+            String line = null;
+            FSDataOutputStream outputStream = fs.create(new Path(args[1] + "/finalResults.txt"));
+            while ((line = reader.readLine()) != null)
+            {
+                String[] keyval = line.split("\\t");
+                String outputLine = String.format("%s\n_______________________\n%s\n", keyval[0], keyval[1].replace("|", "\n"));
+                outputStream.writeChars(outputLine);
+
+            }
+            outputStream.flush();
+            outputStream.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
 }
