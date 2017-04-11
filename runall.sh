20bb87e3301b1948e2517ca33689a47a40ad8552
David Edwards <david@honeycrisp.local>
2017-04-11 16:06:15 -0600

    Script to run all

---
+++ runall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q1.Q1Job /data/census /home/cs455/q1/q1results
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q2.Q2Job /data/census /home/cs455/q2/q2results
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q3.Q3Job /data/census /home/cs455/q3/q3results
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q4.Q4Job /data/census /home/cs455/q4/q4results
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q5.Q5Job /data/census /home/cs455/q5/q5results
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q6.Q6Job /data/census /home/cs455/q6/q6results
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q7.Q7Job /data/census /home/cs455/q7/q7results
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q8.Q8Job /data/census /home/cs455/q8/q8results
+hadoop jar dist/hw3_hadoop.jar cs455.hadoop.q9.Q9Job /data/census /home/cs455/q9/q9results
+
